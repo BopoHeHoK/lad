87c2b85d417a13901168d351620d2d0129e35e9e
Dmitry Korolyov <dimakorolev2030@gmail.com>
2022-08-12 23:00:59 +0300

    Factoring

---
+++ Task2/script.js
@@ -0,0 +1,7 @@
+
+setTimeout(() => {
+    let formElement = document.forms['formElement'];
+
+    formElement.addEventListener("focusin", () => event.target.classList.add('focused'));
+    formElement.addEventListener("focusout", () => event.target.classList.remove('focused'));
+}, 100);
